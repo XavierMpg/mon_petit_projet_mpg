c1fe98499f76b9a70cc2172b941c07276dcf226f
XavierMpg <119058339+XavierMpg@users.noreply.github.com>
2023-04-25 22:58:43 +0200

    Update Streamlit_MPG_22042023.py

--- Streamlit_MPG_22042023.py
+++ Streamlit_MPG_22042023.py
@@ -89,7 +89,7 @@ def dataviz():
 # Page Data des Joueurs Trois sections
 def visualisation():
     st.title("Mes Petits Gardiens")
-    data = pd.read_csv('https://raw.githubusercontent.com/XavierMpg/mon_petit_projet_mpg/main/df_goalkeeper_output_v210423.csv')
+    data = pd.read_csv('https://raw.githubusercontent.com/XavierMpg/mon_petit_projet_mpg/main/df_goalkeeper_output_v250423.csv')
     joueurs = list(data['Joueur'].unique())
     selected_joueur_gk = st.selectbox("Sélectionnez un joueur", joueurs)
     joueur_data = data[data['Joueur'] == selected_joueur_gk]
@@ -99,7 +99,7 @@ def visualisation():
     st.write(selected_variables)
     
     st.title("Mes Petits Joueurs Défensifs")
-    data = pd.read_csv('https://raw.githubusercontent.com/XavierMpg/mon_petit_projet_mpg/main/df_defense_output_v210423.csv')
+    data = pd.read_csv('https://raw.githubusercontent.com/XavierMpg/mon_petit_projet_mpg/main/df_defense_output_v250423.csv')
     joueurs = list(data['Joueur'].unique())
     selected_joueur_def = st.selectbox("Sélectionnez un joueur", joueurs)
     joueur_data = data[data['Joueur'] == selected_joueur_def]
@@ -109,7 +109,7 @@ def visualisation():
     st.write(selected_variables)
 
     st.title("Mes Petits Joueurs Offensifs")
-    data = pd.read_csv('https://raw.githubusercontent.com/XavierMpg/mon_petit_projet_mpg/main/df_attack_output_v210423.csv')
+    data = pd.read_csv('https://raw.githubusercontent.com/XavierMpg/mon_petit_projet_mpg/main/df_attack_output_v250423.csv')
     joueurs = list(data['Joueur'].unique())
     selected_joueur_att = st.selectbox("Sélectionnez un joueur", joueurs)
     joueur_data = data[data['Joueur'] == selected_joueur_att]
